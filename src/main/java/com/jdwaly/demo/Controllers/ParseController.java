c55cc5208458585eacbbfd9938c3fe6f96c74817
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-09-06 13:49:17 +0300

    init

--- src/main/java/com/jdwaly/demo/Controllers/ParseController.java
+++ src/main/java/com/jdwaly/demo/Controllers/ParseController.java
@@ -2,11 +2,18 @@ package com.jdwaly.demo.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+//import org.jsoup.*;
+//import org.jsoup.helper.*;
+//import org.jsoup.nodes.*;
+//import org.jsoup.select.*;
+
 
 @RestController
 public class ParseController {
     @GetMapping("/parse")
     public String ParseCourses(){
-     return "Hello"  ;
+//        Document doc = Jsoup.connect("http://example.com/").get();
+//        String title = doc.title();
+        return "Hiii";
     }
 }
